8877242e8a8b06e6f0f658758f806ad9d02f2c00
Hiten Sidapara <hitensidapara195@gmail.com>
2019-11-24 09:50:50 +0530

    Complete FlaskBlog Website

---
+++ flaskblog/__init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '44037ac8df8938ebea2bdf0d6d7b92e3'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+loginmanager = LoginManager(app)
+loginmanager.login_view = 'login'
+loginmanager.login_message_category = 'info'
+mail = Mail(app)
+
+from flaskblog import routes
